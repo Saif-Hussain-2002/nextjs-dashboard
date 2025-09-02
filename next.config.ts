085e20b647e51d586078843d3466c25ce59a0bf7
Saif-Hussain-2002 <saif-hussain@glixentech.com>
2025-09-02 11:39:04 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- webpack: (config) => {
-    config.resolve.alias['@'] = __dirname;
-    return config;
-  },
-  };
+  /* config options here */
+};
 
 export default nextConfig;
